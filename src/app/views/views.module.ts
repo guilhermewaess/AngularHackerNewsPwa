c2c838d3c6092a980a238208071e4f745f5d7184
Guilherme Waess <Guilherme_Waess@epam.com>
2018-02-13 16:11:49 +0100

    WIP: card styling

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -6,11 +6,13 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { Routes, RouterModule } from '@angular/router';
 import { HackerNewsService } from '../services/hacker-news.service';
 import { FeedItemComponent } from './feed-item/feed-item.component';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
+    MaterialModule,
     AngularFireModule.initializeApp({ databaseURL: 'https://hacker-news.firebaseio.com' }),
     AngularFireDatabaseModule,
   ],
