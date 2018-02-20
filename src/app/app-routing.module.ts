fce1b1a33e5b49e7a2568161e412b58b5e16ca99
Guilherme Waess <Guilherme_Waess@epam.com>
2018-02-20 12:05:01 +0100

    Add animations on router and nav style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,11 @@ import { UserDetailComponent } from './views/user-detail/user-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'new', pathMatch: 'full' },
-  { path: 'top', data: { feed: 'topstories' }, component: FeedComponent },
-  { path: 'new', data: { feed: 'newstories' }, component: FeedComponent },
-  { path: 'ask', data: { feed: 'askstories' }, component: FeedComponent },
-  { path: 'show', data: { feed: 'showstories' }, component: FeedComponent },
-  { path: 'jobs', data: { feed: 'jobstories' }, component: FeedComponent },
+  { path: 'top', data: { feed: 'topstories', state: 'top' }, component: FeedComponent },
+  { path: 'new', data: { feed: 'newstories', state: 'new' }, component: FeedComponent },
+  { path: 'ask', data: { feed: 'askstories', state: 'ask' }, component: FeedComponent },
+  { path: 'show', data: { feed: 'showstories', state: 'show' }, component: FeedComponent },
+  { path: 'jobs', data: { feed: 'jobstories', state: 'jobs' }, component: FeedComponent },
   // { path: 'item/:id', component: StoryDetailComponent },
   { path: 'user/:userId', component: UserDetailComponent },
 ];
