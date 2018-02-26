320dbdc46b8a4eb3c1274e8eefe057e55d014c1b
Guilherme Waess <Guilherme_Waess@epam.com>
2018-02-26 16:00:55 +0100

    WIP: Services unit tests

--- src/app/services/hacker-news.service.spec.ts
+++ src/app/services/hacker-news.service.spec.ts
@@ -1,15 +1,26 @@
-import { TestBed, inject } from '@angular/core/testing';
-
 import { HackerNewsService } from './hacker-news.service';
+import { Observable } from 'rxjs/Observable';
+
+let service: HackerNewsService;
+let angularFirebaseStub;
 
 describe('HackerNewsService', () => {
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [HackerNewsService]
-    });
+    angularFirebaseStub = {
+      list: jasmine.createSpy().and.returnValue({ valueChanges: () => Observable.of([1, 2, 3]) })
+    };
+    service = new HackerNewsService(angularFirebaseStub);
   });
 
-  it('should be created', inject([HackerNewsService], (service: HackerNewsService) => {
-    expect(service).toBeTruthy();
-  }));
+  fdescribe('getFeed', () => {
+    beforeEach(() => {
+    });
+    it('should call fireBase with arguments', () => {
+      service.getFeed('feed1');
+      expect(angularFirebaseStub.list).toHaveBeenCalledWith('/v0/feed1');
+    });
+    it('should return firebase valueChanges', () => {
+      service.getFeed('feed1').subscribe(result => expect(result).toEqual([1, 2, 3]));
+    });
+  });
 });
